ebc4e4d62e3072c7b66cd2ac414c1683d7d75061
MuthuAvinash17 <muthuavi2000@gmail.com>
2024-12-26 11:33:03 +0530

    init commit

---
+++ src/noobChain/Block.java
@@ -0,0 +1,85 @@
+package noobChain;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+
+    public Block(String previousHash){
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        merkleRoot
+        );
+        return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
